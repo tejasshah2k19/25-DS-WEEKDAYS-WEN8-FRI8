380fbbeb48439291adec8b2304576ff5db7ada92
Tejas Shah <tejasshah2k19@gmail.com>
2025-10-05 18:56:10 +0530

    delete last , add beg , del beg

--- SingleLinkList.c
+++ SingleLinkList.c
@@ -40,6 +40,40 @@ void displayList()
     }
 }
 
+void removeNode()
+{
+    struct node *p;
+    p = head;
+
+    while (p->next != last)
+    {
+        p = p->next;
+    }
+    p->next = NULL;
+    free(last);
+    last = p;
+}
+
+void addNodeBEG(int data)
+{
+    struct node *tmp;
+    if (head == NULL)
+    {
+        addNode(data);
+    }
+    else
+    {
+        tmp = malloc(sizeof(struct node));
+        tmp->data = data;
+        tmp->next = head;
+        head = tmp;
+    }
+}
+
+void removeNodeBEG(){
+    //release 
+}
+
 int main()
 {
 
@@ -57,12 +91,14 @@ int main()
     while (1)
     {
         printf("\n0 For Exit");
-        printf("\n1 For Add Node \n2 For List All Nodes\n3 For Remove Node\nEnter choice");
+        printf("\n1 For Add Node \n2 For List All Nodes\n3 For Remove Node\n4 for Add Node : Beg\n5 For Remove : BEG\nEnter choice");
 
         scanf("%d", &choice);
 
         switch (choice)
         {
+        case 0:
+            exit(0);
         case 1:
             printf("\nEnter number");
             scanf("%d", &num);
@@ -71,9 +107,17 @@ int main()
         case 2:
             displayList();
             break;
-        case 0:
-            exit(0);
-
+        case 3:
+            removeNode();
+            break;
+        case 4:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            addNodeBEG(num);
+            break;
+        case 5:
+            removeNodeBEG();
+            break;
         default:
             break;
         }
