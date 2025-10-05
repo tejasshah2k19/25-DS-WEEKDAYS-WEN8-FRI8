7ee93df71ae280e0302aa8a098ba1223b314beed
Tejas Shah <tejasshah2k19@gmail.com>
2025-10-05 18:04:37 +0530

    single link list menu , display

--- SingleLinkList.c
+++ SingleLinkList.c
@@ -27,16 +27,57 @@ void addNode(int data)
     }
 }
 
+void displayList()
+{
+
+    struct node *p;
+    p = head;
+
+    while (p != NULL)
+    {
+        printf(" %d", p->data); // 10  20  30
+        p = p->next;
+    }
+}
+
 int main()
 {
 
-    addNode(10);
-    addNode(20);
-    addNode(30);
-    addNode(40);
-    addNode(50);
+    // addNode(10);
+    // addNode(20);
+    // addNode(30);
+    // addNode(40);
+    // addNode(50);
 
-    printf(" %d  %d  %d  %d ",head->data,head->next->data,head->next->next->data,head->next->next->next->data);
+    // printf(" %d  %d  %d  %d ",head->data,head->next->data,head->next->next->data,head->next->next->next->data);
+    // displayList();
+
+    int choice, num;
+
+    while (1)
+    {
+        printf("\n0 For Exit");
+        printf("\n1 For Add Node \n2 For List All Nodes\n3 For Remove Node\nEnter choice");
+
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            addNode(num);
+            break;
+        case 2:
+            displayList();
+            break;
+        case 0:
+            exit(0);
+
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+
+    // addNode(10);
+    // addNode(20);
+    // addNode(30);
+    // addNode(40);
+    // addNode(50);
